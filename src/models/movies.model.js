a2010c2fa7a5b12ed5be0a09c927ffb16c7f3015
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-07-17 03:33:10 +0530

    Update movies.model.js

--- src/models/movies.model.js
+++ src/models/movies.model.js
@@ -6,7 +6,8 @@ const productSchema = new mongoose.Schema(
     by: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: String, required: true },
-    image: { type: String, required: true }
+    image: { type: String, required: true },
+    category: { type: String, required: true },
   },
   {
     versionKey: false,
