399ca65f6d5e5cd3702b66178f3292844653d4d0
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-07-17 03:53:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+var cors = require("cors");
 
 const movieController = require("./controllers/movieController");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/products", movieController);
 
